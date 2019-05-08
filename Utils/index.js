3cf49426388a9dc32cc2b6262e4a341611c6cb3e
dudueasy <dudueasy@126.com>
2019-05-08 20:42:34 +0800

    项目引入 PropTypes 和 checkPropTypes , 并将其应用于测试

--- Utils/index.js
+++ Utils/index.js
@@ -1,3 +1,14 @@
+import checkPropTypes from 'check-prop-types';
+
 export const findByDataTest = (wrapper, attr) => {
   return wrapper.find(`[data-test='${attr}']`);
+};
+
+export const checkProps = (component, expectedProps) => {
+  return checkPropTypes(
+    component.propTypes,
+    expectedProps,
+    'props',
+    component.name,
+  );
 };
--- src/component/headline/index.js
+++ src/component/headline/index.js
@@ -1,20 +1,42 @@
 import React, {Component} from 'react';
+import * as PropTypes from 'prop-types';
 
 class Headline extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     const {header, desc} = this.props;
 
-    return header ? (
+    return (
       <div data-test="HeadlineComponent">
-        <h1 data-test="header">{header}</h1>
-        <p data-test="desc">{desc}</p>
+
+        {header ?
+          <h1 data-test="header">{header}</h1>
+          : null
+        }
+
+        {desc ?
+          <p data-test="desc">{desc}</p> :
+          null
+        }
       </div>
-    ) : null;
+    );
   }
 }
+
+Headline.propTypes = {
+  header: PropTypes.string,
+  desc: PropTypes.string,
+  tempArr: PropTypes.arrayOf(PropTypes.shape({
+    fName: PropTypes.string,
+    lName: PropTypes.string,
+    email: PropTypes.string,
+    age: PropTypes.number,
+    onlineStatus: PropTypes.bool,
+  })),
+};
+
 
 export default Headline;
